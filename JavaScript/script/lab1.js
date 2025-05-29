65280be2f8b8f8a72551b8c902e6a678931a577a
Carlo <carlo.nicolo@hotmail.com>
2025-05-29 22:41:42 +0200

    Corso Javascript

---
+++ JavaScript/script/lab1.js
@@ -0,0 +1,7 @@
+function greeting(name){
+    const elementoSaluto = document.getElementById('saluto');
+    elementoSaluto.textContent = `Ciao, ${name}! Benvenuto nella nostra pagina.`;
+    
+}
+
+document.addEventListener('DOMContentLoaded', greeting("Daniel"));
